e2373e6450fa553fd96c3f247035bc0b785a48ad
hafizfarman <161736789+hafizfarman@users.noreply.github.com>
2024-03-01 00:04:08 +0500

    Add files via upload

---
+++ windowLoad.js
@@ -0,0 +1,15 @@
+window.addEventListener('load', function () {
+  const pName = document.getElementById('pName');
+  pName.style.display = 'none';
+  const sName = this.document.getElementById('sName');
+  sName.style.display = 'none';
+});
+
+window.addEventListener('load', function () {
+  const topicList = this.document.getElementById('frontPage');
+  topicList.style.display = 'block';
+  const slideBox = this.document.getElementById('slideBox');
+  slideBox.style.display = 'none';
+});
+
+//const topicLists
